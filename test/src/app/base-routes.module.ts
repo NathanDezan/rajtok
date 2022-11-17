d2613f9d19c901710386668aeff6ae8d9ff91da4
beatrizmcosta <beatrizmcosta1@gmail.com>
2022-11-16 23:47:24 -0400

    adicionando playlist list component

--- test/src/app/base-routes.module.ts
+++ test/src/app/base-routes.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { HomepageComponent } from './homepage/homepage.component';
+import { PlaylistComponentComponent } from './playlist-component/playlist-component.component';
+import { PlaylistListComponent } from './playlist-list/playlist-list.component';
 import { RegisterComponent } from './register/register.component';
 
 export default RouterModule.forRoot([
   { path: '', component: HomepageComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'playlists', component: PlaylistListComponent}
 ]);
